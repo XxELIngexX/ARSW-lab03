0ce67b9aca1829eea02bcd8acb3ccb16a25c888f
unknown <miguel.tellez-a@labinfo.is.escuelaing.edu.co>
2024-08-29 09:50:21 -0500

    intento de punto 1

--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -32,15 +32,21 @@ public class Producer extends Thread {
     public void run() {
         while (true) {
 
+            synchronized (queue){
+                try {
+                    if (queue.size() == stockLimit){
+                        queue.wait();
+                    }
+
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+            }
+
             dataSeed = dataSeed + rand.nextInt(100);
             System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
-            }
 
         }
     }
